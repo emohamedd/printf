f5a8673c1fd039a8135f2b40ac5730bb9ed33d3f
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-21 09:08:54 -0700

    create the BUFF SIZE 1024

--- main.h
+++ main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/* includes the lib we need */
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <math.h>
+
+/*containes all the defines */
+
+#define BUFF_SIZE 1024
+
+
+/* the prototype of all function we use to handle printf */
 int _octal(unsigned int x);
 int _putunbr(unsigned int x);
 long _tobinary(unsigned int x);
