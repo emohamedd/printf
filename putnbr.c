c7e280d93b1be851570ec51f82bc0dc3c07ce5c9
emohamedd <a.elgaouej@gmail.com>
2023-04-21 01:44:15 -0700

    first aux

---
+++ putnbr.c
@@ -0,0 +1,28 @@
+#include <main.h>
+
+/* BY EMOHAMEDD AND ABDELGHNI HAMANAR*/
+
+/**
+ *_putnbr - print the nb
+ *@n: the n to print
+ *Return: returns
+ */
+
+void _putnbr(int n)
+{
+	int c = 0;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n > 9)
+	{
+		_putnbr(n / 10);
+		_putnbr(n % 10);
+	}
+	else
+		_putchar(n + '0');
+
+}
