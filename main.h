12e480bb369905a565ce54ac8b283002d700f2ea
emohamedd <a.elgaouej@gmail.com>
2023-04-21 02:12:20 -0700

    Finish TAsk

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+int _putchar(char c);
+char _putstr(char *s;
+void _putnbr(int n);
+#endif
---
+++ printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/* BY EMOHAMEDD AND ABDELGHNI HAMANAR*/
+
+/**
+ *_printf - print character
+ *@format: the character to print
+ *Return: returns
+ */
+
+int check(va_list list, char c)
+{
+	if (c == 'c')
+		return (_putchar(va_arg(list, int)));
+	else if (c == 's')
+		return (_putstr(va_arg(list, char *)));
+	else if (c == 'd' || c == 'i')
+		return (_putnbr(va_arg(list, int)));
+	return (0);
+}
+
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	va_list ptr;
+
+	va_start(ptr, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+				_putchar('%');
+			check(ptr, str[i]);
+		}
+		else
+			_putchar(format[i]);
+			i++;
+	}
+	va_end(ptr);
+}
