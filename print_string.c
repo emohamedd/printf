febe86d9429f243f16a1d8c807a7a5b1bcf03125
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-22 13:05:11 -0700

    check 0

--- print_string.c
+++ print_string.c
@@ -10,12 +10,12 @@
 
 int _print_string(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        write(1, &s[i], 1);
-        i++;
-    }
-    return (i);
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+	return (i);
 }
