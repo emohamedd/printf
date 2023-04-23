b36af6ca0b20d1f4b87c1844f0188c39ce5e673d
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2023-04-23 08:21:36 -0700

    flags

--- main.h
+++ main.h
@@ -10,8 +10,16 @@
 #include <unistd.h>
 
 
-
+#define UNUSED(x) (VOID)(x)
 #define BUFFER 1024
+
+/* FLAGS */
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
 
 
 /* BY EMOHAMEDD AND ABDELGHNI HAMANAR*/
